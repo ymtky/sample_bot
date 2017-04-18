b42a4103d0bc40a8803e87799fc6e1375fec28f7
yamatak <tybjea1348@gmail.com>
2017-04-19 03:21:47 +0900

    add response to reaction

--- src/cron.js
+++ src/cron.js
@@ -1,13 +1,14 @@
 import { CronJob } from "cron"
 
 const TASKS = [
-  { icon : 'book' }
+  { icon : 'book' },
+  { icon : 'keyboard' }
 ]
 
 module.exports = ( robot => {
-  new CronJob('43 * * * *', function() {
-    robot.adapter.client.web.chat.postMessage("#routine", "やった？", {as_user: true}).then(function(res) {
-      TASKS.map(function(task) {
+  new CronJob('01 * * * *', () => {
+    robot.adapter.client.web.chat.postMessage("#routine", "やった？", {as_user: true}).then(res => {
+      TASKS.forEach( task => {
         robot.adapter.client.web.reactions.add(task.icon, {channel: res.channel, timestamp: res.ts})
       })
     })
--- src/sample.js
+++ src/sample.js
@@ -1,5 +1,9 @@
 module.exports = ( robot => {
-  robot.hear(/yeah/, msg => {
-    msg.send("YEAR!!!")
+  robot.react( res => {
+    robot.adapter.client.web.reactions.get({channel: res.message.item.channel, timestamp: res.message.item.ts}).then(res => {
+      if (res.message.reactions.map(r => r.count).lastIndexOf(1) < 0) {
+        robot.send({room: res.channel}, "OK!!")
+      }
+    })
   })
 })
