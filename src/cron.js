47534173c7c874404f8d813d30d575b117e2ce7e
yamatak <tybjea1348@gmail.com>
2017-04-22 00:30:31 +0900

    fix message

--- src/cron.js
+++ src/cron.js
@@ -2,15 +2,34 @@ import { CronJob } from "cron"
 
 const TASKS = [
   { icon : 'book' },
-  { icon : 'keyboard' }
+  { icon : 'english_reading' },
+  { icon : 'keyboard' },
+  { icon : 'octcat' },
+  { icon : 'dusty_stick' }
 ]
 
+const targetChannel = "#routine"
+var lastTimeStamp = ""
+var channelId = ""
+
 module.exports = ( robot => {
-  new CronJob('01 * * * *', () => {
-    robot.adapter.client.web.chat.postMessage("#routine", "やった？", {as_user: true}).then(res => {
+  new CronJob('16 * * * *', () => {
+    robot.adapter.client.web.chat.postMessage(targetChannel, "日課はやった？", {as_user: true}).then(res => {
       TASKS.forEach( task => {
         robot.adapter.client.web.reactions.add(task.icon, {channel: res.channel, timestamp: res.ts})
       })
+      lastTimeStamp = res.ts
+      channelId = res.channel
     })
+  }).start()
+
+  new CronJob('17 * * * *', () => {
+    if (lastTimeStamp && channelId) {
+      robot.adapter.client.web.reactions.get({channel: channelId, timestamp: lastTimeStamp}).then(res => {
+        if (res.message.reactions.map(r => r.count).lastIndexOf(1) >= 0) {
+          robot.send({room: res.channel}, "明日は頑張ろうね")
+        }
+      })
+    }
   }).start()
 })
--- src/sample.js
+++ src/sample.js
@@ -2,7 +2,7 @@ module.exports = ( robot => {
   robot.react( res => {
     robot.adapter.client.web.reactions.get({channel: res.message.item.channel, timestamp: res.message.item.ts}).then(res => {
       if (res.message.reactions.map(r => r.count).lastIndexOf(1) < 0) {
-        robot.send({room: res.channel}, "OK!!")
+        robot.send({room: res.channel}, "よくやった！明日も頑張ろう！")
       }
     })
   })
